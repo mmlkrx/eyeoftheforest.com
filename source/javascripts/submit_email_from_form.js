1db1737b174ad4de61fc70c90c8f1cfa9f40f6b9
Matthias Loker <mmloker@gmail.com>
2017-01-30 18:42:12 +0100

    Revert "Automatically play video on email submit"
    This reverts commit 717eda0d46459ae37fc72c75aa818111ad8e28dc.

--- source/javascripts/submit_email_from_form.js
+++ source/javascripts/submit_email_from_form.js
@@ -7,26 +7,23 @@ $(document).ready(function() {
     // Stop the browser from submitting the form.
     event.preventDefault();
 
-    if ($('#email').val() !== '') {
-      // Serialize the form data.
-      var formData = $(form).serialize();
+    // Serialize the form data.
+    var formData = $(form).serialize();
 
-      // Submit the form using AJAX.
-      $.ajax({
-        type: 'POST',
-        url: $(form).attr('action'),
-        data: formData
-        }).done(function(response) {
-          $('.logo').hide();
-          $('.copy').hide();
-          $('.email').hide();
-          $('.social-media').hide();
-          $('.teaser').fadeIn();
-          setInterval(function(){
-            $('input[type=password]').value = '#EOTF';
-            $('input[type=submit]').click();
-          }, 100);
-        });
-    }
-  });
+    // Submit the form using AJAX.
+    $.ajax({
+      type: 'POST',
+      url: $(form).attr('action'),
+      data: formData
+      }).done(function(response) {
+        $('.logo').hide();
+        $('.copy').hide();
+        $('.email').hide();
+        $('.social-media').hide();
+        $('.teaser').fadeIn();
+        $('#pw').fadeIn();
+        $('.container').css('background-color', '#000000');
+        $('.container').css('background-image', 'none');
+      });
+    });
 });
